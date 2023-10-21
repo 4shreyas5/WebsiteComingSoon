6fb3db8a62cbbcfa7be95a41e71f8e6e9f9e3b19
Shreyas <shreyassingh621@gmail.com>
2023-10-21 21:46:07 +0530

    Initial Commit

---
+++ websitecomingsoon/src/App.js
@@ -0,0 +1,13 @@
+import WebsiteCS from "./components/WebsiteCS";
+
+function App() {
+  return (
+    <div className="App">
+      <WebsiteCS/>
+
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ websitecomingsoon/src/components/WebsiteCS.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function WebsiteCS(){
+    return(
+        <div>
+            <img src="background.png"></img>
+            <div>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+export default WebsiteCS;
